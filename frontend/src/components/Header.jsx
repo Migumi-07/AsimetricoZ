309ce74a9ec795ebbc6c9a21ebef7d4ee96bb235
Tadmor16 <omgdelsur@gmail.com>
2026-05-14 23:31:45 -0500

    Modifiqué el Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="nav-links">
+          <a href="#inicio" className="nav-link">
+            Inicio
+          </a>
+          <a href="#login" className="nav-link">
+            Login
+          </a>
+          <a href="#home" className="nav-link">
+            Home
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import Header from "components/Header";
+import Footer from "components/Footer";
+
 const Home = () => {
   return (
     <>
-      <h1>Pagina si del Gym</h1>
+        <Header />
 
+      <h1>Pagina si del Gym</h1>
+      
+        <Footer />
 
     </>
   );
